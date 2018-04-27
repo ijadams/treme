4babc368a143800519d28ce0c6e3d69bddfcb6ea
Ian Adams <ian.adams@ge.com>
2018-04-27 09:33:21 -0500

    add food content to home box

--- resources/views/partials/home-details.blade.php
+++ resources/views/partials/home-details.blade.php
@@ -4,7 +4,8 @@
     <a href="tel:(504)301-4441" class="phone"><i class="fa fa-phone-square" aria-hidden="true"></i></a><br>
   </span>
   <span class="hours">
-        Thursday - Sunday &middot; 5pm - 2am
+        Thursday - Sunday &middot; 4pm til &middot;
+        <span class="food">food every night</span>
   </span>
 </div>
 

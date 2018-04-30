15dddf26c75b513b725d2d7c2b76403433cd65a7
Ian Adams <ian.adams@ge.com>
2018-04-30 15:01:33 -0500

    add only future events and make logo smol

--- resources/views/partials/events-page.blade.php
+++ resources/views/partials/events-page.blade.php
@@ -1,14 +1,22 @@
 <?php
-/* FEATURED EVENTS LOOP*/
-$query_vars = array(
-  'post_type' => 'events',
-  'posts_per_page'	=> -1,
-  'meta_key'			=> 'date',
-  'orderby'			=> 'meta_value_num',
-  'order'				=> 'ASC'
-);
+  /* FEATURED EVENTS LOOP*/
+  $query_vars = array(
+    'post_type' => 'events',
+    'posts_per_page' => -1,
+    'meta_key' => 'date',
+    'orderby' => 'meta_value_num',
+    'order' => 'ASC',
+    'meta_query' => array(
+      array(
+        'key' => 'date',
+        'type' => 'NUMERIC', // MySQL needs to treat date meta values as numbers
+        'value' => current_time('Ymd'), // Today in ACF datetime format
+        'compare' => '>=', // Greater than or equal to value
+      ),
+    )
+  );
 
-query_posts($query_vars);
+  query_posts($query_vars);
 ?>
 
 <?php
@@ -17,50 +25,50 @@ if (have_posts()) : while (have_posts()) : the_post();
   $date = get_field('date', false, false);
   // make date object
   $date = new DateTime($date);
-  ?>
-    <h1>Upcoming Events</h1>
-    <div class="row">
-      <div class="columns large-12">
-        <section class="events-holder">
-            <article class="single-event">
-              <!-- date -->
-              <div class="event-date">
-                <div class="month">
-                  <?php echo $date->format('M'); ?>
-                </div>
-                <div class="day">
-                  <?php echo $date->format('j'); ?>
-                </div>
+?>
+<h1>Upcoming Events</h1>
+<div class="row">
+  <div class="columns large-12">
+    <section class="events-holder">
+      <article class="single-event">
+        <!-- date -->
+        <div class="event-date">
+          <div class="month">
+            <?php echo $date->format('M'); ?>
+          </div>
+          <div class="day">
+            <?php echo $date->format('j'); ?>
+          </div>
+        </div>
+        <!-- image preview -->
+        <?php if (get_field('image_preview')): ?>
+          <a href="<?php the_field('event_link'); ?>" class="event-image-link">
+            <div class="event-image" style="background-image: url('<?php the_field('image_preview'); ?>');"></div>
+          </a>
+        <?php endif; ?>
+        <!-- information -->
+        <div class="event-info">
+          <div class="title-date-container">
+            <h2><?php echo get_field('event_title'); ?></h2>
+            <?php if (get_field('event_start_time')): ?>
+              <div class="event-time">
+                <span><?php echo get_field('event_start_time'); ?> - <?php echo get_field('event_end_time'); ?></span>
               </div>
-              <!-- image preview -->
-              <?php if( get_field('image_preview') ): ?>
-                <a href="<?php the_field('event_link'); ?>" class="event-image-link">
-                  <div class="event-image" style="background-image: url('<?php the_field('image_preview'); ?>');"></div>
-                </a>
-              <?php endif; ?>
-              <!-- information -->
-              <div class="event-info">
-                <div class="title-date-container">
-                  <h2><?php echo get_field('event_title'); ?></h2>
-                  <?php if( get_field('event_start_time') ): ?>
-                    <div class="event-time">
-                      <span><?php echo get_field('event_start_time'); ?> - <?php echo get_field('event_end_time'); ?></span>
-                    </div>
-                  <?php endif; ?>
-                </div>
-                <div class="event-desc">
-                  <p><?php echo get_field('event_description'); ?></p>
-                </div>
-                <?php if( get_field('event_link') ): ?>
-                  <div class="event-button">
-                    <a href="<?php the_field('event_link'); ?>" class="button">Learn More</a>
-                  </div>
-                <?php endif; ?>
-              </div>
-            </article>
-        </section>
-    </div>
-<?php    endwhile; else : endif; wp_reset_query(); ?>
+            <?php endif; ?>
+          </div>
+          <div class="event-desc">
+            <p><?php echo get_field('event_description'); ?></p>
+          </div>
+          <?php if (get_field('event_link')): ?>
+            <div class="event-button">
+              <a href="<?php the_field('event_link'); ?>" class="button">Learn More</a>
+            </div>
+          <?php endif; ?>
+        </div>
+      </article>
+    </section>
+  </div>
+<?php endwhile; else : endif; wp_reset_query(); ?>
 
 
 

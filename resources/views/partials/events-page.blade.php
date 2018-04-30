81a505331833f2eb0dff026ce2d3ce8bed66c2b3
Ian Adams <ian.adams@ge.com>
2018-04-30 10:00:36 -0500

    smaller logo on mobile menu and hide labels on events if no post

--- resources/views/partials/events-page.blade.php
+++ resources/views/partials/events-page.blade.php
@@ -10,57 +10,58 @@ $query_vars = array(
 
 query_posts($query_vars);
 ?>
-<h1>Upcoming Events</h1>
-<div class="row">
-  <div class="columns large-12">
-    <section class="events-holder">
-      <?php
-      if (have_posts()) : while (have_posts()) : the_post();
-        // get raw date
-        $date = get_field('date', false, false);
-        // make date object
-        $date = new DateTime($date);
-        ?>
-        <article class="single-event">
-          <!-- date -->
-          <div class="event-date">
-            <div class="month">
-              <?php echo $date->format('M'); ?>
-            </div>
-            <div class="day">
-              <?php echo $date->format('j'); ?>
-            </div>
-          </div>
-          <!-- image preview -->
-          <?php if( get_field('image_preview') ): ?>
-            <a href="<?php the_field('event_link'); ?>" class="event-image-link">
-              <div class="event-image" style="background-image: url('<?php the_field('image_preview'); ?>');"></div>
-            </a>
-          <?php endif; ?>
-          <!-- information -->
-          <div class="event-info">
-            <div class="title-date-container">
-              <h2><?php echo get_field('event_title'); ?></h2>
-              <?php if( get_field('event_start_time') ): ?>
-                <div class="event-time">
-                  <span><?php echo get_field('event_start_time'); ?> - <?php echo get_field('event_end_time'); ?></span>
-                </div>
-              <?php endif; ?>
-            </div>
-            <div class="event-desc">
-              <p><?php echo get_field('event_description'); ?></p>
-            </div>
-            <?php if( get_field('event_link') ): ?>
-              <div class="event-button">
-                <a href="<?php the_field('event_link'); ?>" class="button">Learn More</a>
-              </div>
-            <?php endif; ?>
-          </div>
-        </article>
-      <?php    endwhile; else : endif; wp_reset_query(); ?>
-    </section>
 
-</div>
+<?php
+if (have_posts()) : while (have_posts()) : the_post();
+  // get raw date
+  $date = get_field('date', false, false);
+  // make date object
+  $date = new DateTime($date);
+  ?>
+    <h1>Upcoming Events</h1>
+    <div class="row">
+      <div class="columns large-12">
+        <section class="events-holder">
+            <article class="single-event">
+              <!-- date -->
+              <div class="event-date">
+                <div class="month">
+                  <?php echo $date->format('M'); ?>
+                </div>
+                <div class="day">
+                  <?php echo $date->format('j'); ?>
+                </div>
+              </div>
+              <!-- image preview -->
+              <?php if( get_field('image_preview') ): ?>
+                <a href="<?php the_field('event_link'); ?>" class="event-image-link">
+                  <div class="event-image" style="background-image: url('<?php the_field('image_preview'); ?>');"></div>
+                </a>
+              <?php endif; ?>
+              <!-- information -->
+              <div class="event-info">
+                <div class="title-date-container">
+                  <h2><?php echo get_field('event_title'); ?></h2>
+                  <?php if( get_field('event_start_time') ): ?>
+                    <div class="event-time">
+                      <span><?php echo get_field('event_start_time'); ?> - <?php echo get_field('event_end_time'); ?></span>
+                    </div>
+                  <?php endif; ?>
+                </div>
+                <div class="event-desc">
+                  <p><?php echo get_field('event_description'); ?></p>
+                </div>
+                <?php if( get_field('event_link') ): ?>
+                  <div class="event-button">
+                    <a href="<?php the_field('event_link'); ?>" class="button">Learn More</a>
+                  </div>
+                <?php endif; ?>
+              </div>
+            </article>
+        </section>
+    </div>
+<?php    endwhile; else : endif; wp_reset_query(); ?>
+
 
 
 
--- resources/views/partials/events-weekly.blade.php
+++ resources/views/partials/events-weekly.blade.php
@@ -9,28 +9,27 @@ $query_vars = array(
 
 query_posts($query_vars);
 ?>
-<h1>Weekly Events</h1>
-<div class="row">
-  <div class="columns large-12">
-    <section class="events-holder weekly">
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article class="single-event">
-          <div class="event-date">
-            <div class="month">
-              Every <?php echo get_field('day'); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <h1>Weekly Events</h1>
+  <div class="row">
+    <div class="columns large-12">
+      <section class="events-holder weekly">
+          <article class="single-event">
+            <div class="event-date">
+              <div class="month">
+                Every <?php echo get_field('day'); ?>
+              </div>
             </div>
-          </div>
-          <!-- image preview -->
-          <?php if( get_field('image_preview') ): ?>
-            <a href="<?php the_field('event_link'); ?>" class="event-image-link">
-              <img src="<?php the_field('image_preview'); ?>" style="width: 100%;">
-            </a>
-          <?php endif; ?>
-        </article>
-      <?php    endwhile; else : endif; wp_reset_query(); ?>
-    </section>
-
-  </div>
-
+            <!-- image preview -->
+            <?php if( get_field('image_preview') ): ?>
+              <a href="<?php the_field('event_link'); ?>" class="event-image-link">
+                <img src="<?php the_field('image_preview'); ?>" style="width: 100%;">
+              </a>
+            <?php endif; ?>
+          </article>
+      </section>
+    </div>
+  <?php    endwhile; else : endif; wp_reset_query(); ?>
 
 
